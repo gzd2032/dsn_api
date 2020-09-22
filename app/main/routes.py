0185e69cd02720fab571d2f356c5436784368d14
gzd2032 <gzd2032@gmail.com>
2020-09-22 20:49:18 +0200

    updated html homepage

--- app/main/routes.py
+++ app/main/routes.py
@@ -70,49 +70,6 @@ def add_location():
             'locations': locations
         })
 
-@main.route('/locations/<int:location_id>', methods=['PATCH'])
-def update_location(location_id):
-    userinput = request.get_json()
-    if 'name' not in userinput:
-        abort(400)    
-    try:
-        query = Locations.query.filter(Locations.id == location_id).one_or_none()
-        if query is not None:
-            query.name = userinput['name']
-            new_location = query.format()
-            query.update()  
-            success = True 
-        else:
-            new_location = "location not found"
-            success = False
-    except:
-        abort (500)
-    finally:
-        return jsonify({
-            'success': success,
-            'locations': new_location
-        })
-
-@main.route('/locations/<int:location_id>', methods=['DELETE'])
-def delete_locations(location_id):
-    try:
-        query = Locations.query.filter(Locations.id == location_id).one_or_none()
-        if query is not None:
-            removed = query.format()
-            query.delete()
-            success = True
-        else:
-            removed = "not found"
-            success = False
-    except:
-        db.session.rollback()
-        abort (500)
-    finally:
-        return jsonify({
-            'success': success,
-            'prefix': removed
-        })
-
 @main.route('/prefixes', methods=['GET'])
 def get_prefixes():
     try:
@@ -147,39 +104,6 @@ def get_comm_list():
     finally:
         return jsonify({
             'comm': dsns
-        })
-
-@main.route('/prefixes/<int:prefix_id>', methods=['PATCH'])
-def update_prefix(prefix_id):
-    userinput = request.get_json()
-    if 'dsn_prefix' not in userinput:
-        abort(400)
-    dsn = userinput['dsn_prefix']
-    if 'comm_prefix' not in userinput:
-        abort(400)
-    comm = userinput['comm_prefix']
-    if 'location_id' not in userinput:
-        abort(400)
-    location = userinput['location_id']
-    description = userinput['description']
-    try:
-        query = Prefixes.query.filter(Prefixes.id == prefix_id).one_or_none()
-        if query is not None:
-            query.comm_prefix = comm
-            query.description = description
-            query.location_id = location
-            new_prefix = query.format()
-            query.update()
-            success = True
-        else:
-            new_prefix = "prefix not found"
-            success = False
-    except:
-        abort (500)
-    finally:
-        return jsonify({
-            'success': success,
-            'prefix_list': new_prefix
         })
 
 @main.route('/prefixes/dsn/<dsn_prefix>', methods=['GET'])
@@ -239,25 +163,101 @@ def add_prefix():
             'prefix_list': dsns
         })
 
-@main.route('/prefixes/<int:prefix_id>', methods=['DELETE'])
-def delete_prefixes(prefix_id):
-    try:
-        query = Prefixes.query.filter(Prefixes.id == prefix_id).one_or_none()
-        if query is not None:
-            removed = query.format()
-            query.delete()
-            success = True
-        else:
-            removed = "not found"
-            success = False
-    except:
-        db.session.rollback()
-        abort (500)
-    finally:
-        return jsonify({
-            'success': success,
-            'prefix': removed
-        })
+# @main.route('/locations/<int:location_id>', methods=['PATCH'])
+# def update_location(location_id):
+#     userinput = request.get_json()
+#     if 'name' not in userinput:
+#         abort(400)    
+#     try:
+#         query = Locations.query.filter(Locations.id == location_id).one_or_none()
+#         if query is not None:
+#             query.name = userinput['name']
+#             new_location = query.format()
+#             query.update()  
+#             success = True 
+#         else:
+#             new_location = "location not found"
+#             success = False
+#     except:
+#         abort (500)
+#     finally:
+#         return jsonify({
+#             'success': success,
+#             'locations': new_location
+#         })
+
+# @main.route('/prefixes/<int:prefix_id>', methods=['PATCH'])
+# def update_prefix(prefix_id):
+#     userinput = request.get_json()
+#     if 'dsn_prefix' not in userinput:
+#         abort(400)
+#     dsn = userinput['dsn_prefix']
+#     if 'comm_prefix' not in userinput:
+#         abort(400)
+#     comm = userinput['comm_prefix']
+#     if 'location_id' not in userinput:
+#         abort(400)
+#     location = userinput['location_id']
+#     description = userinput['description']
+#     try:
+#         query = Prefixes.query.filter(Prefixes.id == prefix_id).one_or_none()
+#         if query is not None:
+#             query.comm_prefix = comm
+#             query.description = description
+#             query.location_id = location
+#             new_prefix = query.format()
+#             query.update()
+#             success = True
+#         else:
+#             new_prefix = "prefix not found"
+#             success = False
+#     except:
+#         abort (500)
+#     finally:
+#         return jsonify({
+#             'success': success,
+#             'prefix_list': new_prefix
+#         })
+
+# @main.route('/locations/<int:location_id>', methods=['DELETE'])
+# def delete_locations(location_id):
+#     try:
+#         query = Locations.query.filter(Locations.id == location_id).one_or_none()
+#         if query is not None:
+#             removed = query.format()
+#             query.delete()
+#             success = True
+#         else:
+#             removed = "not found"
+#             success = False
+#     except:
+#         db.session.rollback()
+#         abort (500)
+#     finally:
+#         return jsonify({
+#             'success': success,
+#             'prefix': removed
+#         })
+
+# @main.route('/prefixes/<int:prefix_id>', methods=['DELETE'])
+# def delete_prefixes(prefix_id):
+#     try:
+#         query = Prefixes.query.filter(Prefixes.id == prefix_id).one_or_none()
+#         if query is not None:
+#             removed = query.format()
+#             query.delete()
+#             success = True
+#         else:
+#             removed = "not found"
+#             success = False
+#     except:
+#         db.session.rollback()
+#         abort (500)
+#     finally:
+#         return jsonify({
+#             'success': success,
+#             'prefix': removed
+#         })
 
 @main.teardown_request
 def teardown_request(exception):
